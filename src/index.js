163becb4c5f101f986596b2d8f0217ecdf3f132c
iburistu <zachary.linkletter@maine.edu>
2020-03-10 22:52:08 -0400

    Add directory change functions and added basic directory open dialog
    - Refactor static directory setting code to functional components
    - Add basic directory open dialog

--- src/index.js
+++ src/index.js
@@ -1,8 +1,104 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+const isMac = process.platform === 'darwin';
+
+const template = [
+    // { role: 'appMenu' }
+    ...(isMac
+        ? [
+              {
+                  label: app.name,
+                  submenu: [
+                      { role: 'about' },
+                      { type: 'separator' },
+                      { role: 'services' },
+                      { type: 'separator' },
+                      { role: 'hide' },
+                      { role: 'hideothers' },
+                      { role: 'unhide' },
+                      { type: 'separator' },
+                      { role: 'quit' },
+                  ],
+              },
+          ]
+        : []),
+    // { role: 'fileMenu' }
+    {
+        label: 'File',
+        submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
+    },
+    // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            ...(isMac
+                ? [
+                      { role: 'pasteAndMatchStyle' },
+                      { role: 'delete' },
+                      { role: 'selectAll' },
+                      { type: 'separator' },
+                      {
+                          label: 'Speech',
+                          submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }],
+                      },
+                  ]
+                : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
+        ],
+    },
+    // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' },
+        ],
+    },
+    // { role: 'windowMenu' }
+    {
+        label: 'Window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'zoom' },
+            ...(isMac
+                ? [{ type: 'separator' }, { role: 'front' }, { type: 'separator' }, { role: 'window' }]
+                : [{ role: 'close' }]),
+        ],
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: async () => {
+                    const { shell } = require('electron');
+                    await shell.openExternal('https://electronjs.org');
+                },
+            },
+        ],
+    },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+// Global so that other processes can access the root menu
+let main_win;
 
 function createMainWindow() {
     // Create the browser window.
-    const main_win = new BrowserWindow({
+    main_win = new BrowserWindow({
         width: 1200,
         height: 900,
         backgroundColor: '#1e1f26',
@@ -15,8 +111,6 @@ function createMainWindow() {
 
     // and load the index.html of the app.
     main_win.loadFile('index.html');
-
-    //main_win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
--- src/render.js
+++ src/render.js
@@ -1,45 +1,67 @@
-const directory_header = document.getElementById('directory-value');
-const directory_contents = document.getElementById('directory-contents');
-const footer_pwd = document.getElementById('pwd');
-
+const { remote } = require('electron');
+const { dialog } = require('electron').remote;
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
 
+const main_win = remote.getCurrentWindow();
+
 // Get the current directory name (only the folder name)
-directory_header.innerText = process
-    .cwd()
-    .split('\\')
-    .slice(-1)[0];
+const set_directory_header = directory => {
+    document.getElementById('directory-value').innerText = directory.split('\\').slice(-1)[0];
+};
 
 // Footer has entire directory string
-footer_pwd.innerText = process.cwd();
+const set_footer_text = text => {
+    document.getElementById('pwd').innerText = text;
+};
 
 // Get the current directory contents and add them to the sidebar
-fs.readdir(process.cwd(), function(err, files) {
-    if (err) {
-        return console.error('Unable to scan directory: ' + err);
-    }
-    // Sort the files
-    files.sort().forEach(function(file) {
-        let li = document.createElement('li');
-        let text = document.createElement('span');
-        text.innerText = file.toString();
-        // !TODO make directory detection less flakey
-        if (!path.extname(path.join(__dirname, file.toString())) && !file.toString().includes('.')) {
-            li.classList.add('directory-dir');
-            // Is this the best way to add more text?
-            text.innerText = ' - ' + text.innerText + '/';
-        } else {
-            li.classList.add('directory-file');
+const get_current_directory_contents = dir => {
+    fs.readdir(dir, function(err, files) {
+        if (err) {
+            return console.error('Unable to scan directory: ' + err);
         }
-        // !TODO add additional movie file extensions here?
-        // Should I be checking MIME types?
-        if (path.extname(path.join(__dirname, file.toString())) === '.mp4') {
-            li.classList.add('movie-file');
-        }
+        // Sort the files
+        files.sort().forEach(function(file) {
+            let li = document.createElement('li');
+            let text = document.createElement('span');
+            text.innerText = file.toString();
+            // !TODO make directory detection less flakey
+            if (!path.extname(path.join(__dirname, file.toString())) && !file.toString().includes('.')) {
+                li.classList.add('directory-dir');
+                // Is this the best way to add more text?
+                text.innerText = ' - ' + text.innerText + '/';
+            } else {
+                text.classList.add('directory-file');
+            }
+            // !TODO add additional movie file extensions here?
+            // Should I be checking MIME types?
+            if (path.extname(path.join(__dirname, file.toString())) === '.mp4') {
+                li.classList.add('movie-file');
+            }
 
-        li.appendChild(text);
-        directory_contents.appendChild(li);
+            li.appendChild(text);
+            document.getElementById('directory-contents').appendChild(li);
+        });
     });
-});
+};
+
+const open_directory_dialog = () => {
+    let options = {
+        title: 'Open directory...',
+        properties: ['openDirectory'],
+    };
+
+    dialog.showOpenDialog(main_win, options).then(dir => {
+        if (dir.filePaths !== undefined) {
+            console.log(dir.filePaths);
+            set_directory_header(dir.filePaths[0]);
+            set_footer_text(dir.filePaths[0]);
+            get_current_directory_contents(dir.filePaths[0]);
+            process.chdir(dir.filePaths[0]);
+        }
+    });
+};
+
+open_directory_dialog();
