a03bdb51d94c374e1bcf6c41fca168a38e5d0155
iburistu <zachary.linkletter@maine.edu>
2020-05-04 20:17:24 -0400

    Fix security issue

--- test/spec.js
+++ test/spec.js
@@ -3,26 +3,31 @@ const assert = require('assert');
 const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
 const path = require('path');
 
+const app = new Application({
+    path: electronPath,
+    args: [path.join(__dirname, '..', '.webpack', 'main', 'index.js')],
+});
+
 describe('Application launch', function () {
     this.timeout('60s');
 
-    beforeEach(function () {
-        this.app = new Application({
-            path: electronPath,
-            args: [path.join(__dirname, '..', '.webpack', 'main', 'index.js')],
-        });
-        return this.app.start();
+    this.beforeAll(() => {
+        return app.start();
     });
 
-    afterEach(function () {
-        if (this.app && this.app.isRunning()) {
-            return this.app.stop();
+    this.afterAll(() => {
+        if (app && app.isRunning()) {
+            return app.stop();
         }
     });
 
-    it('shows an initial window', function () {
-        return this.app.client.getWindowCount().then(function (count) {
-            assert.equal(count, 1);
-        });
+    it('shows an initial window', async () => {
+        const count = await app.client.getWindowCount();
+        return assert.equal(count, 1);
+    });
+
+    it('does not have the developer tools open', async () => {
+        const devToolsAreOpen = await app.client.waitUntilWindowLoaded().browserWindow.isDevToolsOpened();
+        return assert.equal(devToolsAreOpen, false);
     });
 });
